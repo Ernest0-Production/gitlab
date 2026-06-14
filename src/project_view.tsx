e151a22b09d9677d99bf2a8420313219b86c00c3
ernest0n <magatar136@gmail.com>
2026-06-14 16:57:47 +0400

    Cache project quicklink loads to skip repeat loading screens.
    Use useCachedPromise in project_view so reopening a project deeplink shows the nav menu from cache while refreshing in the background.

--- src/project_view.tsx
+++ src/project_view.tsx
@@ -1,18 +1,18 @@
 import { Detail, LaunchProps } from "@raycast/api";
-import { usePromise } from "@raycast/utils";
+import { useCachedPromise } from "@raycast/utils";
 import { gitlab } from "./common";
 import { ProjectNavMenusList } from "./components/project_nav";
 import { getErrorMessage } from "./utils";
 
 export default function ProjectViewRoot(props: LaunchProps<{ arguments: Arguments.ProjectView }>) {
   const projectId = Number.parseInt(props.arguments.projectId, 10);
-  const { data: project, isLoading, error } = usePromise(
-    (id: number) => gitlab.getProject(id),
-    [projectId],
-    { execute: projectId > 0 },
-  );
+  const {
+    data: project,
+    isLoading,
+    error,
+  } = useCachedPromise((id: number) => gitlab.getProject(id), [projectId], { execute: projectId > 0 });
 
-  if (isLoading) {
+  if (isLoading && !project) {
     return <Detail isLoading markdown="" />;
   }
   if (error || !project) {
